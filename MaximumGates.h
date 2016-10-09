6e9339c4c4f31bd1de0002cbed9de9b55a8fff5a
Khalil Ahmed <mkhalila@outlook.com>
2016-10-09 05:16:56 +0100

    Added vars to track no. of gates being used and max

--- MaximumGates.h
+++ MaximumGates.h
@@ -5,7 +5,10 @@
 using std::vector;
 
 // TODO: Your answer for question 2 goes here
-int maximumGates(const vector<int> & arrives, const vector<int> & departs) {}
+int maximumGates(const vector<int> & arrives, const vector<int> & departs) {
+	int gates = 0;
+	int maxGates = 0;
+}
 
 // Do not write any code below this line
 
