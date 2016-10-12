3bbade9e15dff18dadf4e5e7ecce171c4254d102
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 05:44:52 +0100

    Set 1 way to make any total using coin 1

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -6,14 +6,19 @@ using std::vector;
 
 // TODO: Your answer for question 1a goes here
 
-int howManyWaysToMake(const int & target, const vector<int> & coins) {
+int howManyWaysToMake(const int & total, const vector<int> & coins) {
 	//Create a table using vectors, with a row for every coin
 	vector< vector<int> > table(coins.size());
 
 	//1 way to make 0 for every coin
 	for (int i = 0; i < table.size(); ++i) {
-        table[i] = vector<int>(target);
+        table[i] = vector<int>(total);
         table[i][0] = 1;
+    }
+
+    //Set one way to make any total using coin 1 
+    for (int i = 0; i < total; ++i) {
+        table[0][i] = 1;
     }
 
 }
