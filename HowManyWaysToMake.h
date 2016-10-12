0a51f3069a5519ccf3acab78a9afa01a6d991a91
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 20:14:56 +0100

    Made copy a direct copy of coins and then resized copy to avoid discarding const qualifier

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -26,7 +26,8 @@ int howManyWaysToMake(const int & total, const vector<int> & coins) {
 		return 1;
 	}
 
-	vector<int> copy = coins.resize(coins.size() - 1);
+	vector<int> copy = coins;
+	copy.resize(coins.size() - 1);
 
 	return howManyWaysToMake(total, copy) + howManyWaysToMake(total - coins[coins.size() - 1], coins);
 }
