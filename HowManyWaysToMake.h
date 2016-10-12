dcf8721281cfd737904b3f89702810c718c6d870
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 06:53:35 +0100

    Handled case where coin stack is empty

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -7,6 +7,9 @@ using std::vector;
 // TODO: Your answer for question 1a goes here
 
 int howManyWaysToMake(const int & total, const vector<int> & coins) {
+
+	if (coins.size() == 0) return 0;
+	
 	//Create a table using vectors, with a row for every coin
 	vector< vector<int> > table(coins.size());
 
@@ -20,6 +23,8 @@ int howManyWaysToMake(const int & total, const vector<int> & coins) {
     for (int i = 0; i < total; ++i) {
         table[0][i] = 1;
     }
+
+
 
 }
 
