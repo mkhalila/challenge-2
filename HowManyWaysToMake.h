a202e22732a76ea3859a7318b616b2110c78aec4
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 19:08:36 +0100

    Commented citation for formula to be used and a brief description

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -7,7 +7,16 @@ using std::vector;
 // TODO: Your answer for question 1a goes here
 
 int howManyWaysToMake(const int & total, const vector<int> & coins) {
+	// This method was implemented using the mathematical recursive formula:
+	// f(n, k) = 0, if k < 1 or n < 0
+	//			 1, if n = 0
+	// f(n, k-1) + f(n - ak, k), else
+	// Where m is the target value, k is the coins stack, and ak is a coin that did reduce the target (while maintaining constraints)
+	// Note: Slight amendments of notation have been made due to formatting constraints on this text editor
 
+	// The code below was written by me, using the above formula which I extracted from the following Mathematical paper
+	// written by Andrew Neitsch:
+	// https://andrew.neitsch.ca/publications/m496pres1.nb.pdf
 }
 
 
