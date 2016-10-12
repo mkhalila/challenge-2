fe500009978c7643bd20f9ded909245e50930edd
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 20:29:46 +0100

    Implemented my test for howManyWaysToMake

--- YourTestForHowManyWaysToMake.h
+++ YourTestForHowManyWaysToMake.h
@@ -3,10 +3,22 @@
 
 #include "HowManyWaysToMake.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 
 int testHowManyWaysToMake() {
     // TODO: your test case for question 1b goes inside this function, instead of the next line of code:
-    return 0;
+    const int possibilities = howManyWaysToMake(124, {5, 4, 3, 2, 1});
+    
+    if (possibilities == 103664) {
+        cout << "1) Pass: code correctly returns that there are 103664 ways to make 9 out of {1}\n";     
+        return 0;
+    } else {
+        cout << "1) Fail: code returns that there are " << possibilities << " ways to make 9 out of {1} -- but the answer is 103664\n";     
+        return 1;
+    }
     
 }
 
