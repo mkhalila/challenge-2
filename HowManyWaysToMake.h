6fef6057bdffe8ddb8933332ca1a24a910f05c4d
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 19:05:11 +0100

    Cleared howManyWaysToMake.h - resorting to mathematical formula method - after receving approval

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -8,26 +8,6 @@ using std::vector;
 
 int howManyWaysToMake(const int & total, const vector<int> & coins) {
 
-	if (coins.size() == 0) return 0;
-
-	if (total == 0) return 1;
-
-	//Create a table using vectors, with a row for every coin
-	vector< vector<int> > table(coins.size());
-
-	//1 way to make 0 for every coin
-	for (int i = 0; i < table.size(); ++i) {
-        table[i] = vector<int>(total+1);
-        table[i][0] = 1;
-    }
-
-    //Set one way to make any total using coin 1 
-    for (int i = 0; i < total+1; ++i) {
-        table[0][i] = 1;
-    }
-
-
-
 }
 
 
