4ea92cdc22bc748fd5504e57327d06adca661a1b
Khalil Ahmed <mkhalila@outlook.com>
2016-10-12 05:39:55 +0100

    Created a vector of vectors with a row for every coin

--- HowManyWaysToMake.h
+++ HowManyWaysToMake.h
@@ -1,10 +1,14 @@
 #ifndef HOW_MANY_WAYS_TO_MAKE_H
 #define HOW_MANY_WAYS_TO_MAKE_H
 
+#include <vector>
+using std::vector;
+
 // TODO: Your answer for question 1a goes here
 
 int howManyWaysToMake(const int & target, const vector<int> & coins) {
-
+	//Create a table using vectors, with a row for every coin
+	vector< vector<int> > table(coins.size());
 }
 
 
